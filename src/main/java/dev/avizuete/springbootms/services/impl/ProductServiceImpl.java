688d85ab482459c614cbf1b66f1688d4c52dffad
Antonio Vizuete <antoniovizuete@gmail.com>
2022-03-23 15:31:12 +0100

    Added Cache.

--- src/main/java/dev/avizuete/springbootms/services/impl/ProductServiceImpl.java
+++ src/main/java/dev/avizuete/springbootms/services/impl/ProductServiceImpl.java
@@ -2,6 +2,9 @@ package dev.avizuete.springbootms.services.impl;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import dev.avizuete.springbootms.models.Product;
@@ -16,22 +19,32 @@ public class ProductServiceImpl implements ProductService {
 	private final ProductRepository repository;
 
 	@Override
+	@Cacheable("products")
 	public Iterable<Product> findAll() {
 		return repository.findAll();
 	}
 
 	@Override
+	@Cacheable(value="product", key="#id")
 	public Product findById(final long id) {
 		return repository.findById(id)
 			.orElseThrow(() -> new EntityNotFoundException("Product not found"));
 	}
 
 	@Override
+	@Caching(evict = {			
+			@CacheEvict(value="products", allEntries = true),
+			@CacheEvict(value="product", key="#result.id")
+	})
 	public Product create(final Product product) {
 		return repository.save(product);
 	}
 
 	@Override
+	@Caching(evict = {			
+			@CacheEvict(value="products", allEntries = true),
+			@CacheEvict(value="product", key="#id")
+	})
 	public Product update(final long id, final Product product) {
 		final Product productDb = this.findById(id);
 		productDb.setCategory(product.getCategory());
@@ -43,6 +56,10 @@ public class ProductServiceImpl implements ProductService {
 	}
 
 	@Override
+	@Caching(evict = {			
+			@CacheEvict(value="products", allEntries = true),
+			@CacheEvict(value="product", key="#id")
+	})
 	public void delete(final long id) {
 		repository.deleteById(id);
 	}
